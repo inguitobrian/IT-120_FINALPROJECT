994d3d5fe68ac60c5626543f49ff5ff95619bf6f
Brian Inguito <vincentvangoess@gmail.com>
2024-12-25 23:30:54 +0800

    feat: added send letter to views and path to url
    Co-Authored-By: MarbensonBuyan <132684266+MarbensonBuyan@users.noreply.github.com>
    Co-Authored-By: danielleirishmaymartinez <180589392+danielleirishmaymartinez@users.noreply.github.com>

--- user_app/messaging/urls.py
+++ user_app/messaging/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('dashboard/', views.user_dashboard, name='user_dashboard'),
     path('logout/', views.user_logout, name='user_logout'),
     path('send_message/<int:user_id>/', views.send_message, name='send_message_with_user'),  # With user_id
+     path('send_message_to_santa/', views.send_message_to_santa, name='send_message_to_santa'),
     path('send_message/', views.send_message, name='send_message'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
      path('home/', views.home, name='home'),
